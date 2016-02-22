5452b189a042ecad81c45e29028d9f51e9df0d62
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-02-22 12:08:43 +0000

    Added raycasting object interaction

---
+++ Assets/Scripts/RaycastInteraction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Control/Raycast Interaction")]
+
+public class RaycastInteraction : MonoBehaviour {
+
+    private GameObject objectHit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Ray activationRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(activationRay, out hit)) {
+                Debug.Log("Raycast hit " + hit.transform.tag);
+                if (hit.transform.tag == "cube") {
+                    Debug.Log("Hit cube");
+                }
+            }
+        }
+	}
+}
