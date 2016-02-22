9bb67602af761025a2dcd4a91504bab687058f2f
Stuart McEwan <Stuart McEwan>
2016-02-22 15:45:44 +0000

    Camera class complete with pan and zoom

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -5,14 +5,16 @@ using System.Collections;
 
 public class CameraManager : MonoBehaviour
 {
-	// Debug.Log("(" + cameraPosition.x + ", " + cameraPosition.y + ", " + cameraPosition.z + ")");
+	// Debug.Log( "(" + cameraPosition.x + ", " + cameraPosition.y + ", " + cameraPosition.z + ")" );
 
 	// Public variables
+	public float dragSensitivity; // 0.0025 works well
 	public float zoomSpeed, minZoom, maxZoom;
 	public Camera thisCamera;
 
 	// Private variables
 	private Vector3 cameraPosition;
+	private Vector3 lastMousePosition, currentMousePosition, differenceMousePosition;
 	private bool panning;
 
 	// Use this for initialization
@@ -20,44 +22,61 @@ public class CameraManager : MonoBehaviour
 	{
 		// Initialise camera position to position in Unity Editor
 		cameraPosition = thisCamera.transform.position;
+
+		// Initialise mouse position to origin
+		lastMousePosition = new Vector3 (0, 0, 0);
+		currentMousePosition = new Vector3 (0, 0, 0);
+		differenceMousePosition = new Vector3 (0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		// Update current camera position each frame
+		currentMousePosition = Input.mousePosition;
+
 		Pan();
 		Zoom();
 	}
 
-	// LateUpdate is called once per frame after Update
-	void LateUpdate()
-	{
-		// Update camera position at the end of every frame
-		thisCamera.transform.position = cameraPosition;
-	}
-
 	void Pan()
 	{
+		// Camera translated by difference between current mouse position and last mouse position * -1 * sensitivity * zoom (Drag speed based on zoom)
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			// Set last mouse position to current mouse position for initial click
+			lastMousePosition = currentMousePosition;
+		}
 
+		if  (Input.GetMouseButton(0))
+		{
+			// Calculate difference between current and last mouse position before translate and then update last mouse position to current
+			differenceMousePosition = currentMousePosition - lastMousePosition;
+			thisCamera.gameObject.transform.Translate(differenceMousePosition.x * -1 * dragSensitivity * thisCamera.orthographicSize, differenceMousePosition.y * -1 * dragSensitivity * thisCamera.orthographicSize, 0);
+			lastMousePosition = currentMousePosition;
+		}
 	}
 
 	void Zoom()
 	{
 		if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
 		{
+			// Zoom in
 			thisCamera.orthographicSize -= zoomSpeed;
 		}
 		else if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
 		{
+			// Zoom out
 			thisCamera.orthographicSize += zoomSpeed;
 		}
 
+		// Keep zoom within max and min zoom
 		if(thisCamera.orthographicSize >= maxZoom)
 		{
 			thisCamera.orthographicSize = maxZoom;
 		}
-
-		if(thisCamera.orthographicSize <= minZoom)
+		else if(thisCamera.orthographicSize <= minZoom)
 		{
 			thisCamera.orthographicSize = minZoom;
 		}
