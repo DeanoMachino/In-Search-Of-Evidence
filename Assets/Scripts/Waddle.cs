147e895820302d040fe34a2ab2dae9927880ec79
Stuart McEwan <Stuart McEwan>
2016-02-22 20:43:32 +0000

    Started waddling script (Pingu)

---
+++ Assets/Scripts/Waddle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waddle : MonoBehaviour
+{
+	private Vector3 lastPosition, currentPosition;
+	private bool isWaddling;
+
+	// Use this for initialization
+	void Start ()
+	{
+		lastPosition = this.gameObject.transform.position;
+		currentPosition = this.gameObject.transform.position;
+		isWaddling = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		lastPosition = currentPosition;
+		currentPosition = this.gameObject.transform.position;
+
+		if (currentPosition.x != lastPosition.x || currentPosition.z != lastPosition.z)
+		{
+			isWaddling = true;
+		}
+		else
+		{
+			isWaddling = false;
+		}
+
+		if (isWaddling)
+		{
+			// Waddle logic here
+		}
+	}
+}
