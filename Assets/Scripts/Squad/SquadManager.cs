c1222ff1ad89ffc668e90b52f365918cdc224693
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-02-23 08:16:23 +0000

    Added pinch zoom controls; added UI for squad tasks and task queue.

--- Assets/Scripts/Squad/SquadManager.cs
+++ Assets/Scripts/Squad/SquadManager.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 public class SquadManager : MonoBehaviour {
 
-	public List<GameObject> squadList = new List<GameObject>();
+	public List<GameObject> squadList;
 	public List<Task> taskList = new List<Task>();
 
     // Use this for initialization
@@ -17,6 +17,28 @@ public class SquadManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		CheckTasks();
+	}
+
+	void OnGUI() {
+		// Squad task box
+		GUI.Box(new Rect(85.0f, 0.0f, 200.0f, 25.0f * (squadList.Count + 1)), "Squad (" + GetIdle() + "/" + squadList.Count + ")");
+		for(int i = 0; i < squadList.Count; ++i) {
+			if (squadList[i].GetComponent<SquadMember>().currentTask != null) {
+				GUI.Box(new Rect(85.0f, 25.0f + (i * 25.0f), 200.0f, 25.0f),
+					"Officer " + (i+1) + ": " + squadList[i].GetComponent<SquadMember>().currentTask.type.ToString() + " " + squadList[i].GetComponent<SquadMember>().currentTask.taskObject.transform.tag.ToString());
+			} else {
+				GUI.Box(new Rect(85.0f, 25.0f + (i * 25.0f), 200.0f, 25.0f),
+					"Officer " + (i+1) + ": Idle");
+			}
+		}
+
+		// Task list box
+		GUI.Box(new Rect(Screen.width - 285.0f, 0.0f, 200.0f, 25.0f), "Task Queue (" + taskList.Count + ")");
+		for (int i = 0; i < taskList.Count; ++i) {
+			Debug.Log("Task Queue -- " + taskList.Count);
+			GUI.Box(new Rect(Screen.width - 285.0f, 25.0f + i * 25.0f, 200.0f, 25.0f),
+				(i+1) + ". " + taskList[i].type.ToString() + " " + taskList[i].taskObject.tag);
+		}
 	}
 
 	public void AddTask(GameObject object_, TaskType type) {
@@ -37,5 +59,18 @@ public class SquadManager : MonoBehaviour {
 				g.GetComponent<SquadMember>().GiveTask(t);
 			}
 		}
+	}
+
+	int GetIdle() {
+		// Get the number of squad members currently idle
+		int idle = 0;
+
+		foreach(GameObject g in squadList) {
+			if(g.GetComponent<SquadMember>().currentState == SquadMemberState.IDLE) {
+				++idle;
+			}
+		}
+
+		return idle;
 	}
 }
